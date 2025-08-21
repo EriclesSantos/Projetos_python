e5b49eee34fe606e066144166deabe61edaa9788
Ericles Moura Felipe Dos Santos <133582187+EriclesSantos@users.noreply.github.com>
2025-08-21 10:51:04 -0300

    Delete estrutura_de_repetiçao_while.py

--- estrutura_de_repetiçao_while.py
+++
@@ -1,9 +0,0 @@
-opçao= -1
-
-while opçao != 0:
-    opçao = int(input("[1]Sacar \n[2] Extrato \n [0] Sair \n:"))
-
-    if opçao == 1:
-        print("Sacando...")
-    elif opçao == 2:
-         print("Exibindo Extrato...")
